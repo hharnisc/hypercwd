bcd93e339235662be91ff53dae001f5d21d399af
Harrison Harnisch <hharnisc@gmail.com>
2017-01-08 14:48:55 -0600

    Only dispatches 'SESSION_SET_CWD' when it changes - fixes #13

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const { exec } = require('child_process');
 
 let curPid;
+let curCwd;
 let uids = {};
 
-const setCwd = (pid) =>
-  exec(`lsof -p ${pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, cwd) => {
+const setCwd = (store, pid) =>
+  exec(`lsof -p ${pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, newCwd) => {
     if (err) {
       console.error(err);
     } else {
-      cwd = cwd.trim();
-      store.dispatch({
-        type: 'SESSION_SET_CWD',
-        cwd
-      });
+      const cwd = newCwd.trim();
+      // keep track of curCwd internally
+      // protects against state tree changes
+      if (curCwd !== cwd) {
+        store.dispatch({
+          type: 'SESSION_SET_CWD',
+          cwd,
+        });
+        curCwd = cwd;
+      }
     }
   });
 
 exports.middleware = (store) => (next) => (action) => {
   switch (action.type) {
     case 'SESSION_PTY_DATA':
-      if (curPid && uids[action.uid] === curPid) setCwd(curPid);
+      if (curPid && uids[action.uid] === curPid) setCwd(store, curPid);
       break;
     case 'SESSION_ADD':
       uids[action.uid] = action.pid;
       curPid = action.pid;
-      setCwd(curPid);
+      setCwd(store, curPid);
       break;
     case 'SESSION_SET_ACTIVE':
       curPid = uids[action.uid];
-      setCwd(curPid);
+      setCwd(store, curPid);
       break;
     case 'SESSION_PTY_EXIT':
       delete uids[action.uid];
