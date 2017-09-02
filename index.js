6e6dc94d5cd604ee5e50497880f2a17d49906a33
Harrison Harnisch <hharnisc@gmail.com>
2017-09-02 14:17:06 -0500

    Manage a seperate pid and cwd for each tab

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const { exec } = require('child_process');
 
-let curPid;
 let curCwd;
-let uids = {};
+let tabs = {};
+let curTabId;
 
 // For Windows Support:
 // the final excluded character () is an odd character
 // that was added to the end of the line by posh-hg/posh-git
 const directoryRegex = /([a-zA-Z]:[^\:\[\]\?\"\<\>\|]+)/mi;
 
-const setCwd = (store, action, pid) => {
+const setCwd = (store, action, tabId, forceDispatch) => {
+  const tab = tabs[tabId];
   if (process.platform === 'win32') {
     const newCwd = directoryRegex.exec(action.data);
     if (newCwd) {
       const cwd = newCwd[0];
-      if (curCwd !== cwd) {
+      if (tab.cwd !== cwd && tabId === curTabId) {
         store.dispatch({
           type: 'SESSION_SET_CWD',
           cwd,
         });
-        curCwd = cwd;
       }
+      tab.cwd = cwd;
     }
-  } else if (curPid && uids[action.uid] === curPid) {
-    exec(`lsof -p ${pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, newCwd) => {
+  } else {
+    exec(`lsof -p ${tab.pid} | grep cwd | tr -s ' ' | cut -d ' ' -f9-`, (err, newCwd) => {
       if (err) {
         console.error(err);
       } else {
         const cwd = newCwd.trim();
-        // keep track of curCwd internally
-        // protects against state tree changes
-        if (curCwd !== cwd) {
+        if (tab.cwd !== cwd && tabId === curTabId) {
           store.dispatch({
             type: 'SESSION_SET_CWD',
             cwd,
           });
-          curCwd = cwd;
         }
+        tab.cwd = cwd;
       }
     })
   }
@@ -45,22 +44,27 @@ const setCwd = (store, action, pid) => {
 exports.middleware = (store) => (next) => (action) => {
   switch (action.type) {
     case 'SESSION_PTY_DATA':
-      setCwd(store, action, curPid);
+      setCwd(store, action, action.uid);
       break;
     case 'SESSION_ADD':
-      uids[action.uid] = action.pid;
-      curPid = action.pid;
-      setCwd(store, action, curPid);
+      tabs[action.uid] = {
+        pid: action.pid,
+      };
+      curTabId = action.uid;
+      setCwd(store, action, action.uid);
       break;
     case 'SESSION_SET_ACTIVE':
-      curPid = uids[action.uid];
-      setCwd(store, action, curPid);
+      curTabId = action.uid;
+      store.dispatch({
+        type: 'SESSION_SET_CWD',
+        cwd: tabs[curTabId].cwd,
+      });
       break;
     case 'SESSION_PTY_EXIT':
-      delete uids[action.uid];
+      delete tabs[action.uid];
       break;
     case 'SESSION_USER_EXIT':
-      delete uids[action.uid];
+      delete tabs[action.uid];
       break;
   }
   next(action);
