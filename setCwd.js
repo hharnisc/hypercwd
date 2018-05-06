29a83cc25e58cf59d610c56f6dda4a2f4e13ce8e
Maxim Perepelitsa <n0xff@outlook.com>
2018-05-06 22:11:09 +0700

    Support bash on Windows 10

--- setCwd.js
+++ setCwd.js
@@ -14,21 +14,29 @@ const setCwd = async ({ dispatch, action, tab }) => {
 };
 
 // For Windows Support:
-// the final excluded character () is an odd character
-// that was added to the end of the line by posh-hg/posh-git
-const directoryRegex = /([a-zA-Z]:[^\:\[\]\?\"\<\>\|]+)/mi;
+// the final excluded () and () characters
+// that was added by posh-hg/posh-git and bash
+const reMsShell = /([A-Z]:(([^\s][^\:\[\]\?\"\<\>\|]+)|\\))/mi;
+const reLinuxShell = /\:\s\/mnt\/([A-Z])([^\:\[\]\?\"\<\>\|]+)?/mi;
+
+const parseWindowsCwd = (data) => {
+  const linuxCwd = reLinuxShell.exec(data);
+  if (linuxCwd) return [linuxCwd[1].toUpperCase(), linuxCwd[2] || '/'].join(':');
+
+  const msCwd = reMsShell.exec(data);
+  if (msCwd) return msCwd[0];
+}
 
 const windowsSetCwd = ({ dispatch, action, tab, curTabId }) => {
-  const newCwd = directoryRegex.exec(action.data);
+  const newCwd = parseWindowsCwd(action.data);
   if (newCwd) {
-    const cwd = newCwd[0];
-    if (tab.cwd !== cwd && action.uid === curTabId) {
+    if (tab.cwd !== newCwd && action.uid === curTabId) {
       dispatch({
         type: 'SESSION_SET_CWD',
-        cwd,
+        cwd: newCwd
       });
     }
-    tab.cwd = cwd;
+    tab.cwd = newCwd;
   }
 };
 
